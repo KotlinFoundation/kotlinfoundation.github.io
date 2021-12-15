b8b9bf5bb587c1a8bc6dc28cbab974a6db1bad7f
Yuri Teplyakov <ronnie57x@gmail.com>
2021-12-15 12:37:29 +0300

    ktl-585 feat: header updates

--- src/components/Header/mobile-menu/index.tsx
+++ src/components/Header/mobile-menu/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { MenuTrigger } from "./menu-trigger";
 import { MenuList } from "./menu-list";
 import { MenuItem } from "../types";
+import Helmet from "react-helmet";
 
 type Props = {
   toggleMenu: () => void;
@@ -10,11 +11,27 @@ type Props = {
   whiteBg: boolean;
 };
 
-export const MobileMenu: React.FC<Props> = ({ toggleMenu, isMenuOpened, menuItems, whiteBg }) => {
+export const MobileMenu: React.FC<Props> = ({
+  toggleMenu,
+  isMenuOpened,
+  menuItems,
+  whiteBg,
+}) => {
   return (
     <>
       <MenuTrigger toggleMenu={toggleMenu} isMenuOpened={isMenuOpened} />
-      {isMenuOpened && <MenuList menuItems={menuItems} whiteBg={whiteBg} />}
+      {isMenuOpened && (
+        <>
+          <Helmet>
+            <body className="overlay-locked" />
+          </Helmet>
+          <MenuList
+            menuItems={menuItems}
+            whiteBg={whiteBg}
+            toggleMenu={toggleMenu}
+          />
+        </>
+      )}
     </>
   );
 };
--- src/components/Header/mobile-menu/menu-list/index.tsx
+++ src/components/Header/mobile-menu/menu-list/index.tsx
@@ -1,16 +1,23 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { MenuItem } from "../../index";
+import { MenuItem } from "../../types";
 import * as style from "./menu-list.module.css";
 
 type Props = {
   menuItems: MenuItem[];
   whiteBg: boolean;
+  toggleMenu: () => void;
 };
 
-export const MenuList: React.FC<Props> = ({ menuItems, whiteBg }) => {
+export const MenuList: React.FC<Props> = ({ menuItems, whiteBg, toggleMenu }) => {
+
+  const handleWrapperClick = (e) => {
+    if(e.currentTarget != e.target ) return;
+    toggleMenu();
+  }
+
   return (
-    <nav className={style.wrapper}>
+    <nav className={style.wrapper} onClick={handleWrapperClick}>
       <ul className={`${style.menu} ${whiteBg && style.menuWhiteBg}`}>
         <li>
           <Link
