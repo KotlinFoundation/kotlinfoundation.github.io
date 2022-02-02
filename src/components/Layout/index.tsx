6d7b8d4320e9f1f5a023b69ea4b0e621a99cbe0e
nikpachoo <nikpachoo@gmail.com>
2022-02-02 20:14:17 +0300

    ktl-658 refactor: simplify base layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,8 +1,4 @@
-import { ReactNode} from "react";
-import cn from 'classnames';
-import { MDXProvider} from "@mdx-js/react";
-
-import { CtaBlock } from "@jetbrains/kotlin-web-site-ui/dist/ctaBlock";
+import {FC, ReactNode} from "react";
 
 import "@rescui/typography/lib/font-inter.css";
 import "@jetbrains/kotlin-web-site-ui/dist/typography.css";
@@ -14,105 +10,30 @@ import * as styles from "./layout.module.css";
 import { SEO } from "../Seo";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
-import {Link} from "../Link";
-import {graphql} from "gatsby";
-
-const shortcodes = {
-    p: props => <p {...props} className={cn(props.className, 'ktl-text-1 ktl-offset-bottom-m')}/>,
-    h1: props => <h1 {...props} className={cn(props.className, 'ktl-h1 ktl-offset-top-l ktl-offset-bottom-l')}/>,
-    h2: props => <h2 {...props} className={cn(props.className, 'ktl-h2 ktl-offset-top-l ktl-offset-bottom-m')}/>,
-    h3: props => <h3 {...props} className={cn(props.className, 'ktl-h3 ktl-offset-top-l ktl-offset-bottom-m')}/>,
-    h4: props => <h4 {...props} className={cn(props.className, 'ktl-h4 ktl-offset-top-l ktl-offset-bottom-m')}/>,
-    h5: props => <h5 {...props} className={cn(props.className, 'ktl-h5 ktl-offset-top-l ktl-offset-bottom-m')}/>,
-    h6: props => <h6 {...props} className={cn(props.className, 'ktl-h6 ktl-offset-top-l ktl-offset-bottom-m')}/>,
-    ul: props => <ul {...props} className={cn(props.className, 'ktl-text-1 ktl-offset-top-s')}/>,
-    ol: props => <ol {...props} className={cn(props.className, 'ktl-text-1 ktl-offset-top-s')}/>,
-    li: props => <li {...props} className={cn(props.className, 'ktl-offset-bottom-s')}/>,
-    pre: props => <div><pre {...props} className={cn(props.className, 'ktl-text-2', styles.codeInner)}/></div>,
-    a: props => <Link {...props}/>,
-    img: props => <img {...props}/>,
-
-    // blockquote: props => <blockquote {...props} className={cn(props.className, 'ktl-')}/>,
-    // strong: props => <strong {...props} className={cn(props.className, 'ktl-')}/>,
-    // hr: props => <hr {...props} className={cn(props.className, 'ktl-')}/>,
-    // table: props => <table {...props} className={cn(props.className, 'ktl-')}/>,
-    // tr: props => <tr {...props} className={cn(props.className, 'ktl-')}/>,
-    // td: props => <td {...props} className={cn(props.className, 'ktl-')}/>,
-    // pre: props => <pre {...props} className={cn(props.className, 'ktl-')}/>,
-    //  delete: props => <delete {...props} className={cn(props.className, 'ktl-')}/>,
-    // thematicBreak: props => <thematicBreak {...props} className={cn(props.className, 'ktl-')}/>,
-    //  inlineCode: props => <inlineCode {...props} className={cn(props.className, 'ktl-')}/>,
-};
-
-const contactEmail = 'hello@kotlinfoundation.org';
 
 export interface LayoutProps {
     title: string;
-    intro?: null | ReactNode;
     children: null | ReactNode;
-    outro?: null | ReactNode;
     whiteHeader?: boolean;
-    withoutCta?: boolean;
-    pageContext?: undefined | {
-        frontmatter?: {
-            title?: string
-        }
-    }
 }
 
-export const Layout: React.FC<LayoutProps> = ({
+export const Layout: FC<LayoutProps> = ({
     title,
-    intro = null,
     children,
-    outro = null,
     whiteHeader = false,
-    withoutCta = false,
-    pageContext
 }) => {
-    const pageTitle = pageContext?.frontmatter?.title ?? title;
-
     return (
-        <MDXProvider components={shortcodes}>
-            <SEO title={pageTitle} />
+        <>
+            <SEO title={title} />
             <Header whiteBg={whiteHeader} />
 
-            {intro}
-
             <div className={styles.layout}>
-                <article className="ktl-container">
-                    {children}
-                </article>
+                {children}
 
-                {outro}
-
-                {!withoutCta && (
-                    <CtaBlock
-                        topTitle={<>
-                            Have any questions? <br/>Contact us!
-                        </>}
-                        mainTitle={
-                            <a href={`mailto:${contactEmail}`} className={styles.ctaLink}>{contactEmail}</a>
-                        }
-                    />
-                )}
                 <Footer/>
             </div>
-        </MDXProvider>
+        </>
     );
 };
-
-export const pageQuery = graphql`
-query {
-  allMdx {
-    edges {
-      node {
-        frontmatter {
-          title
-        }
-      }
-    }
-  }
-}
-`
 
 export default Layout;
--- src/pages/structure/index.tsx
+++ src/pages/structure/index.tsx
@@ -7,96 +7,98 @@ import { LinkStandalone } from "../../components/Link";
 
 export default function StructurePage() {
     return (
-        <Layout withoutCta={true} title={'Structure'}>
-            <h1 className={'ktl-h1 ktl-offset-bottom-l ktl-offset-top-xl'}>Structure</h1>
+        <Layout title={'Structure'}>
+            <div className={'ktl-container'}>
+                <h1 className={'ktl-h1 ktl-offset-bottom-l ktl-offset-top-xl'}>Structure</h1>
 
-            <p className={'ktl-text-1 ktl-offset-bottom-l'}>The Kotlin Foundation is a nonprofit nonstock corporation registered in the state of Delaware, USA.</p>
+                <p className={'ktl-text-1 ktl-offset-bottom-l'}>The Kotlin Foundation is a nonprofit nonstock corporation registered in the state of Delaware, USA.</p>
 
-            <Structure className={'ktl-offset-bottom-xxl'}>
-                <Structure.Department
-                    hero
-                    name={'Board of Directors'}
-                    members={[
-                        { name: 'Maxim Shafirov', company: 'JetBrains' },
-                        { name: 'Roman Elizarov', company: 'JetBrains' },
-                        { name: 'Grace Kloba', company: 'Google' },
-                        { name: 'Anwar Ghuloum', company: 'Google' },
-                    ]}
-                >
-                    The Foundation is governed by the Board of Directors that makes all decisions by voting. The Board appoints the Lead Language Designer and the members of the Language Committee.
-                </Structure.Department>
+                <Structure className={'ktl-offset-bottom-xxl'}>
+                    <Structure.Department
+                        hero
+                        name={'Board of Directors'}
+                        members={[
+                            { name: 'Maxim Shafirov', company: 'JetBrains' },
+                            { name: 'Roman Elizarov', company: 'JetBrains' },
+                            { name: 'Grace Kloba', company: 'Google' },
+                            { name: 'Anwar Ghuloum', company: 'Google' },
+                        ]}
+                    >
+                        The Foundation is governed by the Board of Directors that makes all decisions by voting. The Board appoints the Lead Language Designer and the members of the Language Committee.
+                    </Structure.Department>
 
-                <Structure.Department
-                    name={'President'}
-                    members={{ name: 'Maxim Shafirov', company: 'JetBrains' }}
-                >
-                    Elected by the Board of Directors as well. The President acts as the chief officer of the Foundation and supervises its affairs and operations.
-                </Structure.Department>
+                    <Structure.Department
+                        name={'President'}
+                        members={{ name: 'Maxim Shafirov', company: 'JetBrains' }}
+                    >
+                        Elected by the Board of Directors as well. The President acts as the chief officer of the Foundation and supervises its affairs and operations.
+                    </Structure.Department>
 
-                <Structure.Department
-                    name={'Secretary'}
-                    members={{ name: 'Jeffrey van Gogh', company: 'Google' }}
-                >
-                    The Secretary keeps record of all meetings of the Board and its committees, and keeps track of membership and notices to the members, directors and third parties.
-                </Structure.Department>
+                    <Structure.Department
+                        name={'Secretary'}
+                        members={{ name: 'Jeffrey van Gogh', company: 'Google' }}
+                    >
+                        The Secretary keeps record of all meetings of the Board and its committees, and keeps track of membership and notices to the members, directors and third parties.
+                    </Structure.Department>
 
-                <Structure.Department
-                    name={'Lead Language Designer'}
-                    members={{ name: 'Roman Elizarov', company: 'JetBrains' }}
-                >
-                    This person is in charge of all decisions regarding the development of the project and evolution of the language. The Foundation appoints the Lead Language Designer, but does not directly make decisions about the course of the project.
-                </Structure.Department>
+                    <Structure.Department
+                        name={'Lead Language Designer'}
+                        members={{ name: 'Roman Elizarov', company: 'JetBrains' }}
+                    >
+                        This person is in charge of all decisions regarding the development of the project and evolution of the language. The Foundation appoints the Lead Language Designer, but does not directly make decisions about the course of the project.
+                    </Structure.Department>
 
-                <Structure.Department
-                    name={'Language Committee'}
-                    members={[
-                        { name: 'Roman Elizarov', company: 'JetBrains' },
-                        { name: 'Jeffrey van Gogh', company: 'Google' },
-                    ]}
-                >
-                    A special Language Committee appointed by the Foundation reviews the changes proposed by the Lead Language Designer and has the power to reject incompatible changes or require certain deprecation procedures to be carried out.
+                    <Structure.Department
+                        name={'Language Committee'}
+                        members={[
+                            { name: 'Roman Elizarov', company: 'JetBrains' },
+                            { name: 'Jeffrey van Gogh', company: 'Google' },
+                        ]}
+                    >
+                        A special Language Committee appointed by the Foundation reviews the changes proposed by the Lead Language Designer and has the power to reject incompatible changes or require certain deprecation procedures to be carried out.
 
-                    <ul className={cn(styles.list, 'rs-text-2')}>
-                        <li className={'ktl-offset-bottom-xs'}>
-                            <LinkStandalone href="/language-committee-guidelines/">Language Committee</LinkStandalone>
-                        </li>
-                        <li className={'ktl-offset-bottom-xs'}>
-                            <LinkStandalone href="/submitting-incompatible-changes/">Submitting Incompatible changes</LinkStandalone>
-                        </li>
-                    </ul>
-                </Structure.Department>
+                        <ul className={cn(styles.list, 'rs-text-2')}>
+                            <li className={'ktl-offset-bottom-xs'}>
+                                <LinkStandalone href="/language-committee-guidelines/">Language Committee</LinkStandalone>
+                            </li>
+                            <li className={'ktl-offset-bottom-xs'}>
+                                <LinkStandalone href="/submitting-incompatible-changes/">Submitting Incompatible changes</LinkStandalone>
+                            </li>
+                        </ul>
+                    </Structure.Department>
 
-                <Structure.Department
-                    name={'Trademark Committee'}
-                    members={[
-                        { name: 'Hadi Hariri', company: 'JetBrains' },
-                        { name: 'Jan Cichy', company: 'JetBrains' },
-                        { name: 'Alina Dolgikh', company: 'JetBrains' },
-                        { name: 'Hilary Richardson', company: 'Google' },
-                        { name: 'Isabella Fiterman', company: 'Google' },
-                    ]}
-                >
-                    The Trademark Committee is responsible for managing the proper use of the Kotlin trademark, which includes its general purpose use as outlined by the Trademark Usage Guidelines, as well as handling specific use-cases that may fall out of the norm. The Trademark Committee reports directly to the Foundation’s board of directors, albeit mostly has autonomy on matters regarding trademarks.
+                    <Structure.Department
+                        name={'Trademark Committee'}
+                        members={[
+                            { name: 'Hadi Hariri', company: 'JetBrains' },
+                            { name: 'Jan Cichy', company: 'JetBrains' },
+                            { name: 'Alina Dolgikh', company: 'JetBrains' },
+                            { name: 'Hilary Richardson', company: 'Google' },
+                            { name: 'Isabella Fiterman', company: 'Google' },
+                        ]}
+                    >
+                        The Trademark Committee is responsible for managing the proper use of the Kotlin trademark, which includes its general purpose use as outlined by the Trademark Usage Guidelines, as well as handling specific use-cases that may fall out of the norm. The Trademark Committee reports directly to the Foundation’s board of directors, albeit mostly has autonomy on matters regarding trademarks.
 
-                    <ul className={cn(styles.list, 'rs-text-2')}>
-                        <li className={'ktl-offset-bottom-xs'}>
-                            <LinkStandalone href="/guidelines/">Kotlin Brand Usage guidelines</LinkStandalone>
-                        </li>
-                    </ul>
-                </Structure.Department>
+                        <ul className={cn(styles.list, 'rs-text-2')}>
+                            <li className={'ktl-offset-bottom-xs'}>
+                                <LinkStandalone href="/guidelines/">Kotlin Brand Usage guidelines</LinkStandalone>
+                            </li>
+                        </ul>
+                    </Structure.Department>
 
-                <Structure.Department
-                    name={'Operating Committee'}
-                    members={[
-                        { name: 'Jeffrey van Gogh', company: 'Google' },
-                        { name: 'Hadi Hariri', company: 'JetBrains' },
-                        { name: 'John Pampuch', company: 'Google' },
-                        { name: 'Egor Tolstoy', company: 'JetBrains' },
-                    ]}
-                >
-                    The Operating Committee goal is to ensure the functioning of the Kotlin Foundation by executing different operational tasks. The Operating Committee reports directly to the Foundation’s board of directors.
-                </Structure.Department>
-            </Structure>
+                    <Structure.Department
+                        name={'Operating Committee'}
+                        members={[
+                            { name: 'Jeffrey van Gogh', company: 'Google' },
+                            { name: 'Hadi Hariri', company: 'JetBrains' },
+                            { name: 'John Pampuch', company: 'Google' },
+                            { name: 'Egor Tolstoy', company: 'JetBrains' },
+                        ]}
+                    >
+                        The Operating Committee goal is to ensure the functioning of the Kotlin Foundation by executing different operational tasks. The Operating Committee reports directly to the Foundation’s board of directors.
+                    </Structure.Department>
+                </Structure>
+            </div>
         </Layout>
     );
 }
