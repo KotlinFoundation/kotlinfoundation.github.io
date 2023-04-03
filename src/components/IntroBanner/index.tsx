5547c6427d8dac07cb863da981e00ae331e9ac2d
Nikolay Pachkov <nikolay.pachkov@jetbrains.com>
2023-04-03 22:46:21 +0200

    ktl-1055 chore: banner revamp

--- src/components/IntroBanner/index.tsx
+++ src/components/IntroBanner/index.tsx
@@ -2,43 +2,54 @@ import {FC} from "react";
 import cn from "classnames";
 
 import '@jetbrains/kotlin-web-site-ui/out/components/layout/index.css';
+import {useTextStyles} from "@rescui/typography";
 
 import {KtlLayout} from "../KtlLayout";
 
 import introJPG from "./intro/intro.jpg";
 
-import jbLogoSvg from "../../images/jb.svg";
-import googleLogoSvg from "./companies/google.svg";
-
 import * as styles from "./introBanner.module.css";
 
-const SPONSORS = [
-    { name: 'Google', image: googleLogoSvg },
-    { name: 'JetBrains', image: jbLogoSvg },
-];
+interface IntroBannerProps {
+    title: string;
+    members: Array<Group>
+}
 
-export const IntroBanner: FC = ({titleDesktop, titleMobile}) => (
-    <section className={styles.section}>
-        <KtlLayout spacing className={styles.intro}>
-            <h1 className="ktf-h1 ktf-h2--tl">
-                <span className={styles.titleDesktop}>{titleDesktop}</span>
-                <span className={styles.titleMobile}>{titleMobile}</span>
-            </h1>
-            <div className={styles.companies}>
-                <h2 className={cn("ktf-h3 ktf-h4--ts", styles.companiesLabel)}>Сompanies<br/>behind Kotlin</h2>
-                {SPONSORS.map(({name, image}) => {
-                    const id = name[0].toUpperCase() + name.substring(1).toLowerCase();
-                    return (
-                        <p key={id} className={cn(styles.company, styles[`company${id}`])}>
-                            <img alt={`${name} Logo`} src={image} className={styles.companyImage}/>
-                        </p>
-                    );
-                })}
-            </div>
-            <picture className={styles.picture}>
-                {/*<source type="image/webp" srcSet={`${introWebp} 1x, ${intro2xWebp} 2x`} />*/}
-                <img src={introJPG} alt="Funny Kotlin Constructor" className={styles.image}/>
-            </picture>
-        </KtlLayout>
-    </section>
-);
+interface Group {
+    groupName: string;
+    companies: Array<Company>;
+}
+
+interface Company {
+    name: string;
+    image: string;
+}
+
+export const IntroBanner: FC = ({title, members}) => {
+    const textCn = useTextStyles();
+
+    return (
+        <section className={styles.section}>
+            <KtlLayout spacing className={styles.intro}>
+                <img src={introJPG} alt="" className={styles.image}/>
+
+                <h1 className={cn("ktf-h1 ktf-h2--tl ktf-h3--ts", styles.title)}>
+                    {title}
+                </h1>
+
+                <div className={styles.companies}>
+                    {members.map(({groupName, companies}) => (
+                        <div className={styles.companiesGroup} key={groupName}>
+                            <div className={textCn('rs-text-2', {hardness: 'hard'})}>{groupName}</div>
+                            <div className={styles.companiesLogos}>
+                                {companies.map(({name, image}) => (
+                                    <img key={name} alt={`${name} Logo`} src={image} className={styles.companyLogo} />
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </KtlLayout>
+        </section>
+    );
+};
