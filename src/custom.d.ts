4dac4883793a2a80c1889cfd20e2e85906b7f5a7
zoobestik <kb.chernenko@gmail.com>
2023-04-12 17:56:56 +0200

    chore; add *.jpg for typesrcipt

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -3,6 +3,11 @@ declare module '*.svg' {
     export default content;
 }
 
+declare module '*.jpg' {
+    const content: string;
+    export default content;
+}
+
 declare module '*.png' {
     const content: string;
     export default content;
