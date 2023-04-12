90c965ca92a1cbf560891ccc8c88f9416f789b5d
zoobestik <kb.chernenko@gmail.com>
2023-04-12 17:47:57 +0200

    fix; latest news clickabale area

--- src/components/LatestNews/index.tsx
+++ src/components/LatestNews/index.tsx
@@ -1,28 +1,13 @@
-import {FC, useCallback, useMemo} from 'react';
-import {graphql, navigate, useStaticQuery} from "gatsby";
+import {useMemo} from 'react';
+import {graphql, useStaticQuery} from "gatsby";
 import cn from "classnames";
 import {useTextStyles} from "@rescui/typography";
-import {PairedBlock} from "../PairedBlock";
 
-import * as styles from "./latestNews.module.css";
+import {Link} from "../Link";
+import {PairedBlock} from "../PairedBlock";
 import {postContentPreview} from "../PostContent";
 
-const news = [
-    {
-        url: "",
-        date: "January 30, 2023",
-        title: "Results of the Kotlin Multiplatform contest",
-        text: "Lorem ipsum dolor sit amet,duis voluptate velit esse cillum dolore empor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. " +
-            "Lorem ipsum dolor sit amet,duis voluptate velit esse cillum dolore empor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud..."
-    },
-    {
-        url: "",
-        date: "January 22, 2023",
-        title: "Announcing the grants program",
-        text: "Lorem ipsum dolor sit amet,duis voluptate velit esse cillum dolore empor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud. " +
-            "Lorem ipsum dolor sit amet,duis voluptate velit esse cillum dolore empor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud..."
-    }
-];
+import * as styles from "./latestNews.module.css";
 
 export function LatestNews() {
     const textCn = useTextStyles();
@@ -53,37 +38,28 @@ export function LatestNews() {
         <div>
             <h2 className={cn(styles.title, textCn('rs-h2'))}>Latest news</h2>
 
-            <PairedBlock left={
-                <NewsPreview post={posts[0]} />
-            } right={
-                <NewsPreview post={posts[1]} />
-            }></PairedBlock>
+            <PairedBlock
+                left={<NewsPreview post={posts[0]} />}
+                right={<NewsPreview post={posts[1]} />}
+            />
         </div>
     );
 }
 
-interface NewsPreviewProps {
-    date: string;
-    title: string;
-    text: string;
-    url: null | string;
-}
-
-
-const NewsPreview: FC<NewsPreviewProps> = ({post}) => {
+function NewsPreview({ post }) {
     const textCn = useTextStyles();
+    const {date, title} = post.frontmatter;
 
-    const { date, title } = post.frontmatter;
-    const {content, isTrimmed} = useMemo(() => {
-        const link = <span className={cn(styles.more, textCn("rs-link", {hardness: "average"}))}>Read more</span>;
-        return  postContentPreview(post, link);
-    }, [post]);
-    const onReadMore = useCallback(() => {
-        if (post) navigate(post.fields.slug);
+    const {content} = useMemo(() => {
+        const classes = cn(styles.more, textCn("rs-link", {hardness: "average"}));
+
+        return postContentPreview(post, (
+            <Link href={post.fields.slug} className={classes}>Read more</Link>
+        ));
     }, [post]);
 
     return (
-        <div className={styles.previewItem} onClick={onReadMore}>
+        <div className={styles.previewItem}>
             <div className={textCn('rs-text-3')}>{date}</div>
             <h3 className={cn(textCn('rs-h3'), styles.newsTitle)}>{title}</h3>
             <div className={textCn('rs-text-2')}>{content}</div>
