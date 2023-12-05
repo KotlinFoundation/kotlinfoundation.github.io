2907091ae8dbc4b4a86cbade1617dcc2099c1e30
berezinant <berezinant@gmail.com>
2023-12-05 20:53:01 +0100

    KTL-1239 fix: inversed animation after design review, formatted code

--- src/components/AnnualReport/index.tsx
+++ src/components/AnnualReport/index.tsx
@@ -6,35 +6,53 @@ import report2MissionRecap from "./assets/2-mission-recap.png";
 import report3FoundationResults from "./assets/3-foundation-results.png";
 
 export function AnnualReport() {
-    const renderButton = (className: string) => (
-        <Button
-            className={className}
-            mode="outline"
-            size="l"
-            // TODO: upload actual pdf file with the report
-            href="/annual-report-2023.pdf"
-            target="_blank"
-        >
-            View annual report
-        </Button>
-    );
-    return (
-        <div className={styles.report}>
-            <section className={cn("ktl-layout ktl-layout--center", styles.slides)}>
-                <div className={styles.imageContainer}>
-                    <img src={report3FoundationResults} alt="Foundation results" className={styles.image} width={320}
-                         height={180}/>
-                    <img src={report2MissionRecap} alt="Misson recap" className={styles.image} width={320}
-                         height={180}/>
-                    <img src={report1KotlinResults} alt="Kotlin results" className={styles.image} width={320}
-                         height={180}/>
-                </div>
-                <div className={styles.textContainer}>
-                    <h2 className="ktf-h2 ktf-h3--tl">Explore Kotlin Foundation <br/>highlights from 2023</h2>
-                    {renderButton(styles.button)}
-                </div>
-                {renderButton(styles.buttonMobile)}
-            </section>
+  const renderButton = (className: string) => (
+    <Button
+      className={className}
+      mode="outline"
+      size="l"
+      // TODO: upload actual pdf file with the report
+      href="/annual-report-2023.pdf"
+      target="_blank"
+    >
+      View annual report
+    </Button>
+  );
+  return (
+    <div className={styles.report}>
+      <section className={cn("ktl-layout ktl-layout--center", styles.slides)}>
+        <div className={styles.imageContainer}>
+          <img
+            src={report3FoundationResults}
+            alt="Foundation results"
+            className={styles.image}
+            width={320}
+            height={180}
+          />
+          <img
+            src={report2MissionRecap}
+            alt="Misson recap"
+            className={styles.image}
+            width={320}
+            height={180}
+          />
+          <img
+            src={report1KotlinResults}
+            alt="Kotlin results"
+            className={styles.image}
+            width={320}
+            height={180}
+          />
         </div>
-    );
+        <div className={styles.textContainer}>
+          <h2 className="ktf-h2 ktf-h3--tl">
+            Explore Kotlin Foundation <br />
+            highlights from 2023
+          </h2>
+          {renderButton(styles.button)}
+        </div>
+        {renderButton(styles.buttonMobile)}
+      </section>
+    </div>
+  );
 }
