7ec314a961b3be32f608fbef2e3b4894071f3c58
zoobestik <kb.chernenko@gmail.com>
2026-05-19 14:52:14 +0200

    feat(KTL-4488): Add Grants components for application handling

---
+++ src/components/Grants/GrantsAction.tsx
@@ -0,0 +1,54 @@
+import { useTextStyles } from '@jetbrains/kotlin-web-site-ui/out/components/typography';
+import Button from '@rescui/button';
+import { ThemeProvider, useTheme } from '@rescui/ui-contexts';
+import cn from 'classnames';
+import React, { HTMLAttributes, ReactNode } from 'react';
+
+import { cls } from '../../utlis';
+import * as styles from './grants.module.css';
+
+export function Grants(props: HTMLAttributes<HTMLDivElement>) {
+  return <div {...cls(props, styles.grants)} />;
+}
+
+type GrantActionThemeProps = {
+  action: string;
+  url: string;
+  target?: '_blank' | '_self' | '_parent' | '_top';
+  children: ReactNode;
+};
+
+export function GrantsActionTheme({ action, url, children, target = '_blank' }: GrantActionThemeProps) {
+  const theme = useTheme();
+  const textCn = useTextStyles();
+
+  return (
+    <div className={cn(styles.action, textCn('ktl-text-2'), { [styles.actionDark]: theme === 'dark' })}>
+      {children}
+      <Button className={styles.button} mode="outline" size="l" href={url} target={target}>
+        {action}
+      </Button>
+    </div>
+  );
+}
+
+export type GrantsActionProps = GrantActionThemeProps & {
+  theme?: 'dark';
+};
+
+export function GrantsAction({ theme, ...props }: GrantsActionProps) {
+  let content = <GrantsActionTheme {...props} />;
+
+  if (theme) {
+    // this ignore is needed because we can't modify ThemeProvider props and declare `children` prop explicitly as required in react 18
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    content = <ThemeProvider theme={theme}>{content}</ThemeProvider>;
+  }
+
+  return content;
+}
+
+export function GrantsTitle(props: HTMLAttributes<HTMLHeadingElement>) {
+  return <h1 {...cls(props, 'ktf-h2', styles.intro)} />;
+}
